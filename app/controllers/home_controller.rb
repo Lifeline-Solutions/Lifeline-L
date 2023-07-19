4c02e874efe80f325b3687bd9703d2e34320d9be
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-07-19 15:01:28 +0300

    Added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @patient = Patient.all.order('created_at DESC')
-    @patient = Patient.find_by_gender(params[:id])
+    @patients = User.where(role: :patient)
+  
+ 
   end
 end
