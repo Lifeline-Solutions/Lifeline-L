329505ff1a7af2d6b5cd2413668434e55012638b
ger619 <draker74@gmail.com>
2023-07-28 00:14:04 +0300

    add registration details

--- db/migrate/20230716231650_add_name_to_user.rb
+++ db/migrate/20230716231650_add_name_to_user.rb
@@ -4,5 +4,8 @@ class AddNameToUser < ActiveRecord::Migration[7.0]
     add_column :users, :last_name, :string
     add_column :users, :role, :integer, default: 0
     add_column :users, :photo_id, :string
+    add_column :users, :phone_number, :integer
+    add_column :users, :id_number, :string
+
   end
 end
